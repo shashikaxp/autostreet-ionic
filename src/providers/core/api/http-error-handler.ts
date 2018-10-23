4022440175de8b1fe105192a076ad77272797aea
Shashika <Shashika@Shashikas-MacBook-Pro.local>
2018-10-23 23:49:10 +0530

    http error handler

--- src/providers/core/api/http-error-handler.ts
+++ src/providers/core/api/http-error-handler.ts
@@ -1,11 +1,49 @@
 import { Injectable } from '@angular/core';
 import { HttpErrorResponse } from "@angular/common/http";
+import {
+  App, NavController, ToastController
+} from 'ionic-angular';
+import _ from "lodash";
 
 @Injectable()
 export class HttpErrorHandlerProvider {
 
+  private navCtrl: NavController;
+
+  constructor(private toastCtrl: ToastController,
+              private app: App,) {
+    this.navCtrl = app.getActiveNav();
+  }
+
   handle(error: HttpErrorResponse) {
-    console.log('error', JSON.stringify(error));
+    let errorMessage = _.get(error, 'error.message', "Server Error");
+    let status = _.get(error, 'status', "404");
+
+    if (status.toString() === "401") {
+      if (this.navCtrl.getActive().name !== "LoginPage") {
+        this.navCtrl.setRoot("LoginPage", {}, {animate: true, direction: 'forward'})
+      }
+    } else {
+      this.createErrorToast(errorMessage);
+    }
+
+  }
+
+  createErrorToast(message) {
+
+    let options = {
+      message: message,
+      cssClass: "httpErrorToast",
+      position: 'top',
+      showCloseButton: true,
+      dismissOnPageChange: true,
+      closeButtonText: "X"
+    };
+
+    let toast = this.toastCtrl.create(options);
+
+    toast.present();
+
   }
 
 }
