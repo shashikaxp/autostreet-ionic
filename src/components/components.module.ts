61ad74ff988bfee7faf9f7fcdd8fa47fef51dcd6
Shashika <Shashika@Shashikas-MacBook-Pro.local>
2019-03-06 23:46:42 +0530

    new item form component

--- src/components/components.module.ts
+++ src/components/components.module.ts
@@ -4,16 +4,22 @@ import { IonicModule } from "ionic-angular";
 import { IonicImageLoader } from "ionic-image-loader";
 import { CategorySelectorComponent } from './category-selector/category-selector';
 import { FiltersComponent } from './filters/filters';
+import { NewItemFormComponent } from './new-item-form/new-item-form';
+import { FieldModelComponent } from './new-item-form/field-model/field-model';
 
 @NgModule({
   declarations: [CameraImageComponent,
     CategorySelectorComponent,
-    FiltersComponent],
+    FiltersComponent,
+    NewItemFormComponent,
+    FieldModelComponent],
   imports: [IonicModule,
             IonicImageLoader],
   exports: [CameraImageComponent,
     CategorySelectorComponent,
-    FiltersComponent]
+    FiltersComponent,
+    NewItemFormComponent,
+    FieldModelComponent]
 })
 export class ComponentsModule {
 }
