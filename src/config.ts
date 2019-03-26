1ea5860903bbbb008c73a5b65f11186b3b6db282
Shashika <Shashika@Shashikas-MacBook-Pro.local>
2019-03-26 23:22:24 +0530

    set api version

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,10 @@
 export const API_URL = "http://139.162.2.101:8080/";
+export const API_VERSION = 'v1';
+
+export const ITEM_TYPES = {
+  VEHICLE: 'VEHICLE',
+  SPARE_PART: 'SPARE_PART'
+};
 
 export const STORAGE = {
   TOKEN: 'token',
--- src/providers/core/api/http-headers.ts
+++ src/providers/core/api/http-headers.ts
@@ -1,8 +1,10 @@
 import { HttpHeaders } from "@angular/common/http";
+import { API_VERSION } from "../../../config";
 
 export const HEADER_SKIP_INTERCEPTOR = {
   headers: new HttpHeaders({
     'Content-Type': 'application/json',
+    'X-Api-Version': API_VERSION,
     'X-Skip-Interceptor': ''
   })
 };
@@ -10,6 +12,7 @@ export const HEADER_SKIP_INTERCEPTOR = {
 export const HEADER_SKIP_LOADER = {
   headers: new HttpHeaders({
     'Content-Type': 'application/json',
+    'X-Api-Version': API_VERSION,
     'X-Skip-HttpLoader': ''
   })
 };
