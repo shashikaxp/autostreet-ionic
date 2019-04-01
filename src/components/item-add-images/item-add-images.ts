a952c99722ecc7935e09ab10456a9a42cd80350f
Shashika <Shashika@Shashikas-MacBook-Pro.local>
2019-04-01 23:00:33 +0530

    new image add component

---
+++ src/components/item-add-images/item-add-images.ts
@@ -0,0 +1,45 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import _ from 'lodash';
+
+@Component({
+  selector: 'item-add-images',
+  templateUrl: 'item-add-images.html'
+})
+export class ItemAddImagesComponent {
+
+  @Input() imageCount;
+  @Output() selectedImages = new EventEmitter();
+
+  public images;
+
+  constructor() {
+  }
+
+  ngOnInit() {
+    this.images = this.getImagesArray(this.imageCount);
+  }
+
+  getImagesArray(imageCount) {
+    let tempArray = [];
+    for (let i = 0; i < imageCount; i++ ) {
+      tempArray.push({
+        id: i,
+        src: null
+      });
+    }
+    return tempArray;
+  }
+
+  newImage(event, id) {
+    let imageObject = _.find(this.images, {'id': id});
+    if (typeof imageObject !== 'undefined') {
+      imageObject.src = event.src;
+      this.selectedImages.emit({
+        data: _.map(this.images, 'src')
+      });
+    }
+
+  }
+
+
+}
