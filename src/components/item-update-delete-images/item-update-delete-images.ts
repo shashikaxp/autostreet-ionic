ef1c6dae8f9fccc1c9ed7b10d0b191abc77770cf
Shashika <Shashika@Shashikas-MacBook-Pro.local>
2019-04-01 23:00:54 +0530

    new image edit update component

---
+++ src/components/item-update-delete-images/item-update-delete-images.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+
+/**
+ * Generated class for the ItemUpdateDeleteImagesComponent component.
+ *
+ * See https://angular.io/api/core/Component for more info on Angular
+ * Components.
+ */
+@Component({
+  selector: 'item-update-delete-images',
+  templateUrl: 'item-update-delete-images.html'
+})
+export class ItemUpdateDeleteImagesComponent {
+
+  text: string;
+
+  constructor() {
+    console.log('Hello ItemUpdateDeleteImagesComponent Component');
+    this.text = 'Hello World';
+  }
+
+}
