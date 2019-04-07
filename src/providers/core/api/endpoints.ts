73b14984483ede2e728aca8136da8b94d633f4c2
Shashika <Shashika@Shashikas-MacBook-Pro.local>
2019-04-07 19:03:10 +0530

    remove unwanted endpoints

--- src/providers/core/api/endpoints.ts
+++ src/providers/core/api/endpoints.ts
@@ -27,27 +27,7 @@ export const ENDPOINTS = {
     UPDATE_IMAGE: API_URL + "items/{{param1}}/images/{{param2}}",
     DELETE_IMAGE: API_URL + "items/{{param1}}/images/{{param2}}"
   },
-
-  // company endpoints
-  COMPANY: {
-    REGISTER: API_URL + "companies",
-    PARTS_LIST: API_URL + "companies//spare-parts?page={{param2}}&limit={{param3}}"
-  },
-
-  //Spare Parts
-  PARTS: {
-    MANUFACTURERS: API_URL + "manufacturers",
-    MODELS: API_URL + "manufacturers/{{param1}}/models",
-    CATEGORIES: API_URL + "spare-parts/categories",
-    CONDITIONS: API_URL + "spare-parts/conditions",
-    ADD: API_URL + "companies/{{param1}}/spare-parts",
-    DETAILS: API_URL + "companies/{{param1}}/spare-parts/{{param2}}",
-    IMAGE_UPDATE: API_URL + "companies/{{param1}}/spare-parts/{{param2}}/images/{{param3}}",
-    ADD_NEW_IMAGE: API_URL + "companies/{{param1}}/spare-parts/{{param2}}/images",
-    DELETE_IMAGE: API_URL + "companies/{{param1}}/spare-parts/{{param2}}/images/{{param3}}",
-    DETAILS_UPDATE: API_URL + "companies/{{param1}}/spare-parts/{{param2}}"
-  },
-
+  
   SELLERS: {
     REGISTER: API_URL + "sellers",
     ITEMS: API_URL + "sellers/{{param1}}/items?{{param2}}",
